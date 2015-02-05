0b009a1f6c1fb2f5d53db6f813492dfad9ec5337
zoldiqam <aminegriche@MacBook-Pro-de-Amine.local>
2015-02-05 18:27:26 +0000

    Modification au niveau de l'interface : project

--- config/connections.js
+++ config/connections.js
@@ -58,19 +58,8 @@ module.exports.connections = {
   ***************************************************************************/
 
 	 mongodev: {
-<<<<<<< Updated upstream
-=======
         adapter: 'sails-mongo',
         host: 'localhost',
-		port: 27017,
-		database: 'cadroctet_pm_db',
-		schema: true
-},
-
-	mongoprod: {
->>>>>>> Stashed changes
-		adapter: 'sails-mongo',
-		host: 'localhost',
 		port: 27017,
 		database: 'cadroctet_pm_db',
 		schema: true
